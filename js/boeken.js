e9887d83e5ca4eea5433c00b49a178d84d63062d
MaxBouricius <Maxbouricius@hotmail.com>
2020-11-20 15:38:22 +0100

    Stap 5 klaar

--- js/boeken.js
+++ js/boeken.js
@@ -19,12 +19,16 @@ const boeken = {
         this.data.forEach( boek => {
 
 
-            let titel = "";
+            let completeTitel = "";
             if ( boek.voortitel ) {
-                titel += boek.voortitel + " ";
+                completeTitel += boek.voortitel + " ";
             }
-            titel += boek.titel;
-            html += `<h3>${titel}</h3>`;
+            completeTitel += boek.titel;
+
+            html += `<section class="boek">`;
+            html += `<img class="boek__cover" src="${boek.cover}" alt="${completeTitel}">`
+            html += `<h3>${completeTitel}</h3>`;
+            html += `</section>`;
         });
         uitvoer.innerHTML = html
      }
